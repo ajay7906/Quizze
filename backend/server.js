2db3d0f920156767aa6577914f956380204315ff
Ajay Kumar <aks969014@gmail.com>
2024-06-03 18:14:27 +0530

    done changes in bakcend

--- backend/server.js
+++ backend/server.js
@@ -12,8 +12,10 @@ connectDB();
 const app = express();
 app.use(cors())
 
-app.use(express.json());
-app.use(express.json());
+// app.use(express.json());
+// app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 app.use('/api/v1/user', authRoutes);
 app.use('/api/v1/quiz', quizRoutes);
